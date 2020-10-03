b293271431d76c6bba30a6c942196c7a95a1a59e
Perdente <58706003+Perdente@users.noreply.github.com>
2020-10-03 12:20:06 +0600

    Update Sliding Cost.cpp

--- Sorting and Searching/Sliding Cost.cpp
+++ Sorting and Searching/Sliding Cost.cpp
@@ -1,33 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <ext/pb_ds/assoc_container.hpp>
- 
+#include<bits/stdc++.h>
+#define ll long long
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
 using namespace std;
-using namespace __gnu_pbds;
+#define ordered_multiset tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> 
  
-typedef long long ll;
-typedef vector<int> vi;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
- 
-int main() {
-    int n, k;
-    cin >> n >> k;
-    vi a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    indexed_set s;
-    for (int i = 0; i < k; i++) s.insert(a[i]);
-    ll old_m = *s.find_by_order((k + 1) / 2 - 1);
-    ll d = 0;
-    for (int i = 0; i < k; i++) d += abs(a[i] - old_m);
-    cout << d;
-    for (int i = 0; i < n - k; i++) {
-        s.erase(s.find_by_order(s.order_of_key(a[i])));
-        s.insert(a[i + k]);
-        ll m = *s.find_by_order((k + 1)/ 2 - 1);
-        d = d + abs(m - a[i + k]) - abs(old_m - a[i]);
-        if (k % 2 == 0) d -= (m - old_m);
-        old_m = m;
-        cout << " " << d;
-    }
-    cout << endl;
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+   	ll n,m;cin>>n>>m;
+   	ll a[n];
+   	for(int i=0;i<n;++i)
+   	{
+   		cin>>a[i];
+	}
+	ordered_multiset st;
+	for(int i=0;i<m;++i)
+	{
+		st.insert(a[i]);
+	}
+	ll P=*st.find_by_order((m+1)/2-1);
+	ll d=0;
+	for(int i=0;i<m;++i)d+=abs(a[i]-P);
+	cout<<d<<" ";
+	for(int i=0;i<n-m;++i)
+	{
+		st.erase(st.find_by_order(st.order_of_key(a[i])));
+		st.insert(a[i+m]);
+		ll p=*st.find_by_order((m+1)/2-1);
+		d+=abs(p-a[i+m])-abs(P-a[i]);
+		if(!(m&1))d-=p-P;
+		P=p;
+		cout<<d<<" ";
+	}
+	return 0;
 }
