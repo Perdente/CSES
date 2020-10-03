b4ed5ef5d6abcf7c3026ce9e43b68da593fac154
Perdente <58706003+Perdente@users.noreply.github.com>
2020-10-03 11:23:58 +0600

    Update Subarray Sums I.cpp

--- Sorting and Searching/Subarray Sums I.cpp
+++ Sorting and Searching/Subarray Sums I.cpp
@@ -7,34 +7,20 @@ int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     ll n,sum;cin>>n>>sum;
-    vector<ll>v(n),presum(n,0);
+    vector<ll>v(n);
     for(int i=0;i<n;++i)
     	cin>>v[i];
-    presum[0]=v[0];	
-	for(int i=1;i<n;++i)
-		presum[i]=presum[i-1]+v[i];
-	
-	ll j=0,cnt=0;
-	for(int i=0;i<n;++i)
-	{
-		if(presum[i]>sum)
-		{
-			while(j<i)
-			{
-				int p=presum[i]-presum[j];
-				if(p==sum)
-				{
-					cnt++;
-					break;
-				}
-				else if(p<sum)break;
-				j++;
-			}
+    ll m=0,cnt=0;
+    map<ll,ll>mp;
+    for(int i=0;i<n;++i)
+    {
+    	m+=v[i];
+    	if(m==sum)cnt++;
+    	if(mp.count(m-sum))
+    	{
+    		cnt+=mp[m-sum];
 		}
-		else if(presum[i]==sum)
-		{
-			cnt++;
-		}
+		mp[m]++;
 	}
 	cout<<cnt<<endl;
 	return 0;
