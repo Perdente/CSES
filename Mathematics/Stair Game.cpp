13d77fea5fc79531195bc41b885a4744a9c3206c
Perdente <58706003+Perdente@users.noreply.github.com>
2020-10-17 11:04:22 +0600

    Create Stair Game.cpp

---
+++ Mathematics/Stair Game.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1e9+7;
+#define ll long long
+#define mx 1000005
+ 
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;cin>>t;
+    while(t--)
+    {
+    	int n,flag=0;
+    	cin>>n;
+    	vector<int>v(n);
+    	for(int i=0;i<n;++i)
+    	{
+    		cin>>v[i];
+    		if(i&1)flag^=v[i];
+		}
+		cout<<(flag?"first":"second")<<endl;
+	}
+	return 0;
+}
