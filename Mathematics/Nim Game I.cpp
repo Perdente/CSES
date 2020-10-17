25f112fbbd1812a23e9f250160be1c10d1478381
Perdente <58706003+Perdente@users.noreply.github.com>
2020-10-17 11:02:53 +0600

    Create Nim Game I.cpp

---
+++ Mathematics/Nim Game I.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1e9+7;
+#define ll long long
+#define mx 1000005
+ 
+ 
+ 
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;cin>>t;
+    while(t--)
+    {
+    	int n,x=0;
+    	cin>>n;
+    	vector<int>v(n);
+    	for(int &i:v)
+    	{
+    		cin>>i;
+    		x^=i;
+		}
+		cout<<(x?"first":"second")<<endl;
+    	
+	}
+	return 0;
+}
