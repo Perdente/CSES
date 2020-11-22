2a4fe3479261149a5f0b386a8ba88fa143ff4981
Perdente <58706003+Perdente@users.noreply.github.com>
2020-11-22 21:26:05 +0600

    Update Grid Paths.cpp

--- Dynamic Programming/Grid Paths.cpp
+++ Dynamic Programming/Grid Paths.cpp
@@ -1,43 +1,22 @@
 #include<bits/stdc++.h>
+#define int long long int
+#define long long
+const int mod=1e9+7;
 using namespace std;
-const int mod = 1e9+7;
  
-int main()
+signed main()
 {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n;
-    cin>>n;
-    int dp[n][n];
-    char a[n][n];
-    for(int i=0;i<n;++i)
-    {
-    	for(int j=0;j<n;++j)
-    	{
-    		cin>>a[i][j];
+	int n;cin>>n;
+	int dp[n+1];
+	memset(dp,0,sizeof(dp));
+	dp[0]=1;
+	for(int i=1;i<=n;++i)
+	{
+		for(int x=1;x<=6;++x)
+		{
+			if(i-x>=0)dp[i]+=dp[i-x],dp[i]%=mod;
 		}
 	}
-	if(a[0][0]=='*')return cout<<0<<endl,0;
-    memset(dp,0,sizeof(dp));
-    dp[0][0]=1;
-    for(int i=0;i<n;++i)
-    {
-    	for(int j=0;j<n;++j)
-    	{
-    		if(a[i][j]=='.')
-    		{
-    			if(i>0)
-	    		{
-	    			dp[i][j]+=dp[i-1][j];
-	    			dp[i][j]%=mod;
-				}
-				if(j>0)
-				{
-					dp[i][j]+=dp[i][j-1];
-					dp[i][j]%=mod;
-				}
-			}
-		}
-	}
-	cout<<dp[n-1][n-1]<<endl;
-    return 0;
+	cout<<dp[n]<<endl;
+	return 0;
 }
