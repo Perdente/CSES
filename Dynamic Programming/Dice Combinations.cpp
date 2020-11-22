aa1edcb796c08a4d27d79b92515a05dd77d0603f
Perdente <58706003+Perdente@users.noreply.github.com>
2020-11-22 21:10:20 +0600

    Update Dice Combinations.cpp

--- Dynamic Programming/Dice Combinations.cpp
+++ Dynamic Programming/Dice Combinations.cpp
@@ -1,23 +1,22 @@
 #include<bits/stdc++.h>
+#define int long long int
+#define long long
+const int mod=1e9+7;
 using namespace std;
-const int mod = 1e9+7;
  
-int main()
+signed main()
 {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int sum;
-    cin>>sum;
-    int dp[sum+1];
-    memset(dp,0,sizeof(dp));
-    dp[0]=1;
-    for(int i=1;i<=sum;++i)
-    {
-        for(int j=1;j<=6 and (i-j)>=0;++j)
-        {
-        	dp[i]+=dp[i-j];
-        	dp[i]%=mod;
-        }
-    }
-    cout<<dp[sum]<<endl;
-    return 0;
+	int n;cin>>n;
+	int dp[n+1];
+	memset(dp,0,sizeof(dp));
+	dp[0]=1;
+	for(int i=1;i<=n;++i)
+	{
+		for(int x=1;x<=6;++x)
+		{
+			if(i-x>=0)dp[i]+=dp[i-x],dp[i]%=mod;
+		}
+	}
+	cout<<dp[n]<<endl;
+	return 0;
 }
